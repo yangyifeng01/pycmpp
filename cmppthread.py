280bb8a1aca309ef9ca5a2568679e8819b5c862b
yangyifeng01 <yangyifeng_01@163.com>
2014-08-12 23:25:58 +0800

    add a new thread
    resendbox is a class to hold and determine wether the connection is ok.
    And i doubt that if i haven't any communications on the connection last
    for 45 seconds, the connection will b e closed.

--- cmppthread.py
+++ cmppthread.py
@@ -1,20 +1,69 @@
 ﻿#!/bin/usr/env python
-
+# -*- coding: utf-8 -*-
 
 import queue
 import time
 import threading
 import socket
-from cmppdefines import CMPP_CONNECT_RESP, CMPP_SUBMIT_RESP, CMPP_DELIVER, CMPP_QUERY_RESP, CMPP_ACTIVE_TEST, CMPP_ACTIVE_TEST_RESP
+from cmppdefines import CMPP_CONNECT_RESP, CMPP_SUBMIT_RESP, CMPP_DELIVER, CMPP_TERMINATE_RESP, CMPP_QUERY_RESP, CMPP_ACTIVE_TEST, CMPP_ACTIVE_TEST_RESP
+
+class resendbox(threading.Thread):
+
+    def __init__(self, send, terminate, send_list, recv_list, interval = 1, T = 30, N = 3):
+        threading.Thread.__init__(self)
+        self.__resend_list = []
+        self.__send = send
+        self.__send_list = send_list
+        self.__recv_list = recv_list
+        self.__count = 0
+        self.__interval = interval
+        self.__T = T
+        self.__N = N - 1
+        self.__thread_stop = False
+        self.__terminate = terminate
+
+
+    def run(self):
+        while not self.__thread_stop:
+            self.__count += 1
+
+            for sid in self.__recv_list:
+                if sid in self.__send_list:
+                    self.__send_list.remove(sid)
+
+            for resend in self.__resend_list:
+                if resend['seq'] in self.__send_list:
+                    if (self.__count - resend['count']) > self.__T:
+                        if resend['N'] == 0:
+                            self.__terminate()
+                        else:
+                            self.__send(resend['msg'])
+                            resend['N'] -= 1
+                            resend['count'] = self.__count
+                else:
+                    self.__resend_list.remove(resend)
+
+
+            #print(self.__resend_list)
+            time.sleep(self.__interval)
+        return
+
+    def append(self,seq, msg):
+        self.__resend_list.append({'seq': seq,
+            'msg': msg, 'count': self.__count, 'N': self.__N})
+
+    def stop(self):
+        self.__thread_stop = True
 
 class recvthread(threading.Thread):
 
-    def __init__(self ,recv , deliverresp, activeresp, seq_list, interval = 0):
+    def __init__(self, recv, deliverresp, activeresp, send_list, recv_list, interval = 0):
         threading.Thread.__init__(self)
         self.__interval = interval
         self.__thread_stop = False
         self.__recv = recv
-        self.__seq_list = seq_list
+        self.__send_list = send_list
+        self.__recv_list = recv_list
         self.__deliverresp = deliverresp
         self.__activeresp = activeresp
 
@@ -24,12 +73,14 @@ class recvthread(threading.Thread):
             try:
                 h,b = self.__recv()
                 print(h,b)
-                if h['command_id'] in (CMPP_CONNECT_RESP, CMPP_SUBMIT_RESP, CMPP_QUERY_RESP, CMPP_ACTIVE_TEST_RESP):
-                    self.__seq_list.remove(h['sequence_id'])
+                if h['command_id'] in (CMPP_CONNECT_RESP, CMPP_SUBMIT_RESP, CMPP_QUERY_RESP, CMPP_ACTIVE_TEST_RESP, CMPP_TERMINATE_RESP):
+                    self.__recv_list.append(h['sequence_id'])
                 elif h['command_id'] == CMPP_DELIVER:
                     self.__deliverresp(b['Msg_Id'], 0, h['sequence_id'])
+                    self.__recv_list.append(h['sequence_id'])
                 elif h['command_id'] == CMPP_ACTIVE_TEST:
                     self.__activeresp(h['sequence_id'])
+                    self.__recv_list.append(h['sequence_id'])
 
             except socket.error as arg:
                 print(arg)
@@ -42,26 +93,65 @@ class recvthread(threading.Thread):
 
 class sendthread(threading.Thread):
 
-    def __init__(self, send, send_queue, seq_list, interval=0,internal=0, C=180, T=60, N=3):
+    def __init__(self, send, terminate, send_queue, send_list, recv_list, interval = 1):
         threading.Thread.__init__(self)
         self.__interval = interval
         self.__thread_stop = False
         self.__send = send
         self.__send_queue = send_queue
-        self.__seq_list = seq_list
+        self.__send_list = send_list
+        self.__resendbox = resendbox(send, terminate, send_list, recv_list)
 
     def run(self):
         print('send thread start')
+        self.__resendbox.start()
         while not self.__thread_stop:
             try:
-#                while len(self.__seq_list) >=16:
-#                    time.sleep(self.__interval)
-                msg,seq = self.__send_queue.get()
-                self.__send(msg)
-                self.__seq_list.append(seq)
-                print(self.__seq_list)
+                if len(self.__send_list) <16:
+                    msg,seq = self.__send_queue.get()
+                    if type(msg) == type([]):
+                        for index in range(0,len(msg)):
+                            self.__send(msg[index])
+                    else:
+                        self.__send(msg)
+                    self.__send_list.append(seq)
+                    self.__resendbox.append(seq, msg)
+
+                    print(self.__send_list)
+                else:
+                    time.sleep(self.__interval)
             except socket.error as arg:
                 print(arg)
+        return
+
+    def stop(self):
+        self.__resendbox.stop()
+        self.__thread_stop = True
+
+class contactthread(threading.Thread):
+
+    def __init__(self, active, send_queue, interval=1, C=30):
+        threading.Thread.__init__(self)
+        self.__interval = interval
+        self.__thread_stop = False
+        self.__send_queue = send_queue
+        self.__active = active
+        self.__C = C
+
+    def run(self):
+        print('contact thread start')
+        count = 0
+        while not self.__thread_stop:
+            if count < (self.__C*2):
+                if self.__send_queue.qsize() != 0:
+                    count = 0
+                else:
+                    count += 1
+            else:
+                self.__active()
+                count = 0
+            #print('idle:%d' % count)
+            time.sleep(self.__interval/2)
         return
 
     def stop(self):
